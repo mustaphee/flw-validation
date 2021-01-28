b2df4ce309e275534dbbaaae00103acb5647ca74
Yusuff Mustapha <yusuf.m@touchcore.com.ng>
2021-01-28 23:05:00 +0100

    move unhandledrejectionError to server.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const routes = require('./routes');
 const resolveSyntax = require('./middlewares/resolve-syntax-error');
 const { errorResponse } = require('./utils/responses');
@@ -9,17 +11,20 @@ const app = express();
 app.use(express.json());
 // If that fails sometimes, this package middleware handles it properly
 app.use(resolveSyntax());
+//
+app.use(cors());
 
 app.use('/', routes);
 
 // 404 Error Handler
+// eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => errorResponse(res, 404, 'Route doesnt exist.'));
 
-app.use((err, req, res, next) => errorResponse(res, 500, 'Ok, we are being honest here, we don\'t what happened.'));
-
-process.on('unhandledRejection', (error) => {
-  console.error('FATAL UNEXPECTED UNHANDLED REJECTION!', error.message);
-  process.exit(0);
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log(err);
+  return errorResponse(res, 500, 'Ok, we are being honest here, we don\'t what happened.');
 });
 
 module.exports = app;
