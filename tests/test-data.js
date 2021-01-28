98f2f9f3488104d8b1bfb8bb822dd8e5778b4f84
Yusuff Mustapha <yusuf.m@touchcore.com.ng>
2021-01-28 14:01:17 +0100

    setup integration test
    - add test for home endpoint
    - add request for 404 endpoint

---
+++ tests/test-data.js
@@ -0,0 +1,9 @@
+module.exports = {
+  homeData: {
+    name: 'Yusuff Mustapha',
+    github: '@mustaphee',
+    email: 'officialwebdev@gmail.com',
+    mobile: '08160532895',
+    twitter: '@dev_inately',
+  },
+};
