05d4158121d9f9fa61f4d50f72c136db4399c8eb
Yusuff Mustapha <yusuf.m@touchcore.com.ng>
2021-01-28 13:00:17 +0100

    setup all dependencies and config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  plugins: ['jest'],
+};
