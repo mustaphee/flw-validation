6c77e1785d91dba123136325257ed5566fe18c9f
Yusuff Mustapha <yusuf.m@touchcore.com.ng>
2021-01-28 23:03:22 +0100

    change object position

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -1,17 +1,17 @@
 function errorResponse(res, statusCode, message = 'An error occurred', data = null, status = 'error') {
   const responseObject = {
+    message,
     status,
     data,
-    message,
   };
   return res.status(statusCode).send(responseObject);
 }
 
 function successResponse(res, statusCode, message, data = {}, status = 'success') {
   const responseObject = {
+    message,
     status,
     data,
-    message,
   };
   return res.status(statusCode).send(responseObject);
 }
